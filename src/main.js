03967587ffd59ea28a2d00980a0aa82f33a2838f
OlivierBelaud <olivierbelaudpro@gmail.com>
2021-11-05 19:16:34 +0100

    chore: add svelte

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// import App from './App.svelte'
+
+// const app = new App({
+//   target: document.getElementById('app')
+// })
+
+// export default app
